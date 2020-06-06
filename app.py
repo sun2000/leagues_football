08fbf48973ee1c6fb1eb93a70bbb00117dc0848b
haroun <haroun.ouaichouche@gmail.com>
2020-06-06 22:19:41 +0200

    set width for dropdown buttons

--- app.py
+++ app.py
@@ -286,7 +286,7 @@ app.layout = html.Div(
                             value=initial_division_value,
                             style={
                                 # 'height': '5px',
-                                'width': '150%',
+                                'width': '100%',
                                 # 'font-size': "50%",
                                 # 'min-height': '3px',
                             },
@@ -343,7 +343,7 @@ app.layout = html.Div(
                             value=initial_team,
                             style={
                             #     'height': '2px',
-                                'width': '150%',
+                                'width': '100%',
                             #     'font-size': "50%",
                             #     'min-height': '1px',
                             },
