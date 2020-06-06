9ad136e0579fc87b2a146b70d9b67fa35aaed7ce
haroun <haroun.ouaichouche@gmail.com>
2020-06-06 22:50:23 +0200

    set width for dropdown buttons

--- app.py
+++ app.py
@@ -343,7 +343,7 @@ app.layout = html.Div(
                             value=initial_team,
                             style={
                             #     'height': '2px',
-                                'width': '100%',
+                                'width': '110%',
                             #     'font-size': "50%",
                             #     'min-height': '1px',
                             },
